2eb438eb0191d768281bc198510d4705b67ab8ee
Zachary Yaro <zmyaro@zmyaro.com>
2016-05-04 14:30:58 -0400

    Fix semester picker JS being hard coded to missions page

--- static/scripts/semester_picker.js
+++ static/scripts/semester_picker.js
@@ -3,7 +3,7 @@ window.addEventListener('load', function () {
 	var semesterPicker = document.getElementById('semesterPicker');
 	if (semesterPicker) {
 		semesterPicker.semester.onchange = function (e) {
-			window.open('/missions?semester=' + e.target.value, '_self');
+			window.open('?semester=' + e.target.value, '_self');
 		}
 	}
 }, false);
