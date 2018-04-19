3b6179d9e82da5778ab8a6e08913864d49cce48c
Claire Noble <csn6973@g.rit.edu>
2018-04-19 18:11:26 -0400

    added comments to autocomplete file

--- static/scripts/myAutocomplete.js
+++ static/scripts/myAutocomplete.js
@@ -2,9 +2,14 @@ var input;
 var hidden_input;
 var awesomplete;
 var current_members;
+
+/**
+ * The ready function loads in the awesomplete functionality from awesomplete.js
+ * Also preps the autocomplete functionality and can take multiple inputs
+ */
 $(document).ready((function() {
 	input = document.getElementsByClassName("awesomplete").item(1);
-	input.value = input.value.substring(0,input.value.length-1); //removes the last comma
+	input.value = input.value.substring(0,input.value.length-1); //removes the last comma from the input value
 	hidden_input = document.getElementById("runners_id");
 	awesomplete = new Awesomplete(input,{
 		filter: function(text, input) {
@@ -21,6 +26,9 @@ $(document).ready((function() {
 	getMembers();
 }));
 
+/**
+ * Queries the data for members that match what is types. The query given is either everything inside of the input or the last string past the last comma.
+ */
 function getMembers(){
 	var query;
 	if(input.value.includes(",")){
@@ -43,6 +51,10 @@ function getMembers(){
 	input.focus();
 }
 
+/**
+ * Removal functionality so that both the visible and hidden inputs are in sync.
+ * Removes the values that are split by the commas. Also removes the free flowing comma in the hidden input if applicable.
+ */
 function removeMembers(){
 	var seenValArray = input.value.split(",");
 	var hiddenValArray = hidden_input.value.split(",");
@@ -58,6 +70,10 @@ function removeMembers(){
 	}
 }
 
+/**
+ * Adds member if to the hidden input.
+ * @param textLabel
+ */
 function addHiddenMemberID(textLabel){
 	current_members.forEach(function(member){
 		//console.log("mLabel",member.label);
